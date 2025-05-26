3c60a69c117479adc102e5cb4e1dc5b03de28f8a
Alexis Williams <alexis@typedr.at>
2025-05-25 20:41:44 -0700

    Expose all scripts through pyproject.toml

--- src/bentu404_cursors/__init__.py
+++ src/bentu404_cursors/__init__.py
@@ -1,1 +1,3 @@
-from .convertcursors import main
+from .convertcursors import main as convertcursors
+from .downloadcursors import main as downloadcursors
+from .generatepreviews import main as generatepreviews
